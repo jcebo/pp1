71f5d461d3638e8aaceca6e3f99bce26be50fa3f
jcebo <56255697+jcebo@users.noreply.github.com>
2019-10-21 13:05:29 +0200

    Add files via upload

---
+++ 02-ControlStructures/22.py
@@ -0,0 +1,7 @@
+tab=[15,8,31,47,2,19]
+suma=0
+n=len(tab)
+for i in tab:
+    suma+=i
+wynik=suma/n
+print(f"srednia arytmetyczna wynosi: {suma}"
---
+++ 02-ControlStructures/8.py
@@ -0,0 +1,5 @@
+x= input("wprowadz liczbe calkowita x: ")
+y= input("wprowadz liczbe calkowita y: ")
+if (x>y):
+    print(f"Wieksza liczba to: {x} ")
+else: print(f"Wieksza liczba to: {y} ")
